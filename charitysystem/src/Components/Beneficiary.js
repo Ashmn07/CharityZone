e1fdac146a0c7cccbcf5b580c069d1ae80cc4662
Ashmn07 <ashwin.menon202@gmail.com>
2022-05-01 22:29:51 +0530

    Added UI for beneficiary,donor & validator pages

--- charitysystem/src/Components/Beneficiary.js
+++ charitysystem/src/Components/Beneficiary.js
@@ -1,10 +1,24 @@
 import React from 'react'
-import {useLocation} from 'react-router-dom';
+import {useLocation,Link} from 'react-router-dom';
+import useCharity from '../contract/useCharity'
+import Logo from '../assets/logo.png'
 
 function Beneficiary() {
-    const location = useLocation()
+  const location = useLocation()
   return (
-    <div>Hi {location.state.name} you are a beneficiary</div>
+    <div className="bg-black h-screen w-screen">
+      <nav className="border-gray-200 px-10 py-2.5 fixed w-full">
+          <div className="container flex flex-wrap justify-between items-center mx-auto px-6 py-2">
+              <div className="flex items-center">
+                  <img src={Logo} className="mr-3 h-6 sm:h-9" alt="Charity Zone Logo" />
+                  <Link to="/" className="self-center text-4xl font-bold whitespace-nowrap text-white hover:text-gray-300 cursor-pointer">Charity Zone</Link>
+              </div>
+              <div className="text-white">
+                Welcome {location.state.name}
+              </div>
+          </div>
+      </nav>
+    </div>
   )
 }
 
